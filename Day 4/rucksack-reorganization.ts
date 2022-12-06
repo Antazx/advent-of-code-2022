8cbf381be68bd4124cc8a47d7791d60275dedc67
Guillermo Anta <guillermo.anta@telecomboutique.com>
2022-12-06 21:55:39 +0100

    Day3.1 first star

---
+++ Day 4/rucksack-reorganization.ts
@@ -0,0 +1,104 @@
+import readInput from '../readInput.ts';
+
+type Compartment = 'string';
+type Rucksack = [Compartment, Compartment];
+
+const UPPERCASE_CHARS = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+const LOWERCASE_CHARS = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+const ITEM_VALUES = { ...charToValue(LOWERCASE_CHARS), ...charToValue(UPPERCASE_CHARS, 27) };
+
+const input = await readInput('Day 4/input.txt');
+const rucksackList: Rucksack[] = input
+  .split('\n')
+  .map((rucksack) => [
+    rucksack.slice(0, rucksack.length / 2) as Compartment,
+    rucksack.slice(rucksack.length / 2) as Compartment
+  ]);
+
+const commonItems = rucksackList.map(findCommonItem);
+const itemValueList = commonItems.map(itemToValue);
+const result = itemValueList.reduce((acc, curr) => acc + curr, 0);
+console.log('🚀 ~ file: rucksack-reorganization.ts:78 ~ result', result);
+
+function itemToValue(item: string) {
+  const index = item as keyof typeof ITEM_VALUES;
+  return ITEM_VALUES[index];
+}
+
+function charToValue(charList: string[], outset = 1) {
+  return charList.reduce((acc, curr, index) => ({ ...acc, [curr]: index + outset }), {});
+}
+
+function findCommonItem(rucksack: Rucksack): string {
+  const [firstCompartment, secondCompartment] = rucksack;
+  const firstItemList = firstCompartment.split('');
+  const secondItemList = secondCompartment.split('');
+
+  if (firstItemList.length !== secondItemList.length)
+    throw new Error(`Compartments doesn't have same amount of items`);
+
+  for (let itemIndex1 = 0; itemIndex1 < firstItemList.length; itemIndex1++) {
+    for (let itemIndex2 = 0; itemIndex2 < secondItemList.length; itemIndex2++) {
+      const [item1, item2] = [firstItemList[itemIndex1], secondItemList[itemIndex2]];
+      if (item1 === item2) return item1;
+    }
+  }
+
+  throw new Error(`Compartments doesn't have common items`);
+}
