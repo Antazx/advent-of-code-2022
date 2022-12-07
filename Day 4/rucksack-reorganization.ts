6a9f96e0a7d77529947749bde92fbf78baab689a
Guillermo Anta <guillermo.anta@telecomboutique.com>
2022-12-07 18:08:01 +0100

    Day3.2 second star

--- Day 4/rucksack-reorganization.ts
+++ Day 4/rucksack-reorganization.ts
@@ -2,6 +2,10 @@ import readInput from '../readInput.ts';
 
 type Compartment = 'string';
 type Rucksack = [Compartment, Compartment];
+type ElfGroup = {
+  badge: string;
+  rucksacks: [Rucksack, Rucksack, Rucksack];
+};
 
 const UPPERCASE_CHARS = [
   'A',
@@ -70,11 +74,11 @@ const rucksackList: Rucksack[] = input
     rucksack.slice(0, rucksack.length / 2) as Compartment,
     rucksack.slice(rucksack.length / 2) as Compartment
   ]);
-
-const commonItems = rucksackList.map(findCommonItem);
-const itemValueList = commonItems.map(itemToValue);
+let elfGroups: ElfGroup[] = getElfGroups(rucksackList);
+elfGroups = determineBadge(elfGroups);
+const itemValueList = elfGroups.map((group) => group.badge).map(itemToValue);
 const result = itemValueList.reduce((acc, curr) => acc + curr, 0);
-console.log('🚀 ~ file: rucksack-reorganization.ts:78 ~ result', result);
+console.log('🚀 ~ file: rucksack-reorganization.ts:83 ~ result', result);
 
 function itemToValue(item: string) {
   const index = item as keyof typeof ITEM_VALUES;
@@ -101,4 +105,62 @@ function findCommonItem(rucksack: Rucksack): string {
   }
 
   throw new Error(`Compartments doesn't have common items`);
+}
+
+function getElfGroups(rucksackList: Rucksack[]): ElfGroup[] {
+  const elfGroups: ElfGroup[] = [];
+  const exactGroups = rucksackList.length % 3 === 0;
+  if (!exactGroups) throw new Error('Cant get groups of 3 elfs');
+
+  for (let index = 2; index < rucksackList.length; index++) {
+    const rucksackIndex = index + 1;
+    if (rucksackIndex % 3 === 0) {
+      const rucksacks = [rucksackList[index - 2], rucksackList[index - 1], rucksackList[index]] as [
+        Rucksack,
+        Rucksack,
+        Rucksack
+      ];
+      const elfGroup: ElfGroup = { badge: '', rucksacks };
+      elfGroups.push(elfGroup);
+    }
+  }
+
+  return elfGroups;
+}
+
+function determineBadge(elfGroups: ElfGroup[]): ElfGroup[] {
+  for (let index = 0; index < elfGroups.length; index++) {
+    const { rucksacks } = elfGroups[index];
+    elfGroups[index].badge = getBadge(rucksacks);
+  }
+
+  return elfGroups;
+}
+
+function getBadge(rucksacks: Rucksack[]): string {
+  const joinRucksac = rucksacks.map((rucksack) => rucksack.join(''));
+  const mapItems: Record<string, boolean[]> = {};
+
+  for (let rucksackIndex = 0; rucksackIndex < joinRucksac.length; rucksackIndex++) {
+    const items = joinRucksac[rucksackIndex].split('');
+    for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+      const item = items[itemIndex];
+      if (item in mapItems) {
+        mapItems[item][rucksackIndex] = true;
+      } else {
+        mapItems[item] = initValues(rucksackIndex);
+      }
+      if (mapItems[item].filter((item) => item === true).length === 3) return item;
+    }
+  }
+
+  throw new Error('Cant get badge');
+}
+
+function initValues(rucksackIndex: number): boolean[] {
+  const values: boolean[] = [];
+  values.length = 3;
+  values.fill(false);
+  values[rucksackIndex] = true;
+  return values;
 }
