676ce04e7cd387f074faa3a4dca411ee902c7b15
Guillermo Anta <guillermo.anta@telecomboutique.com>
2022-12-11 19:48:43 +0100

    Day4.1 first star ✮

---
+++ Day 4/camp-cleanup.ts
@@ -0,0 +1,41 @@
+import readInput from '../readInput.ts';
+
+type Range = { start: number; end: number };
+type Assignment = [Range, Range];
+
+const inputString = await readInput('Day 4/input.txt');
+const assignments: Assignment[] = parseInput(inputString);
+const contained = assignments.map(checkAssignment);
+const result = contained.filter((isContained) => isContained === true).length;
+console.log('🚀 ~ file: camp-cleanup.ts:10 ~ result', result);
+
+function parseInput(inputString: string): Assignment[] {
+  const assignments: Assignment[] = [];
+  const rows = inputString.split('\n');
+  for (let index = 0; index < rows.length; index++) {
+    const row = rows[index].split(',');
+    assignments.push(toAssignment(row));
+  }
+  return assignments;
+}
+
+function checkAssignment(assignment: Assignment): boolean {
+  const [range1, range2] = assignment;
+  return containsRange(range1, range2) || containsRange(range2, range1);
+}
+
+function containsRange(range1: Range, range2: Range): boolean {
+  return range1.start <= range2.start && range1.end >= range2.end;
+}
+
+function toAssignment(stringList: string[]): Assignment {
+  return stringList.map(toRange) as Assignment;
+}
+
+function toRange(input: string): Range {
+  const [startString, endString] = input.split('-');
+  return {
+    start: Number(startString),
+    end: Number(endString)
+  };
+}
