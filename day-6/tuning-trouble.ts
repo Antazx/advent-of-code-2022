3c1e18c207d06cc1967e26eff02a545d5995aa77
Guillermo Anta <guillermo.anta@telecomboutique.com>
2022-12-13 14:19:10 +0100

    Day6.2 super super  eeeeasy star ✮

--- day-6/tuning-trouble.ts
+++ day-6/tuning-trouble.ts
@@ -1,13 +1,15 @@
 import readInput from '../readInput.ts';
 
+const UNIQUE_CHAR_COUNT = 14;
 const input = await readInput('day-6/input.txt');
-const processedCount = findMarker(input.split(''));
-console.log(`file: tuning-trouble.ts:6 - marker`, processedCount);
+const [processedCount, message] = findMarker(input.split(''));
+console.log(`file: tuning-trouble.ts:6 - marker`, processedCount, message);
 
-function findMarker(input: string[]): number {
-  for (let index = 4; index < input.length; index++) {
-    const candidate = input.slice(index - 4, index);
-    if ([...new Set(candidate)].length === 4) return index;
+function findMarker(input: string[]): [number, string] {
+  for (let index = UNIQUE_CHAR_COUNT; index < input.length; index++) {
+    const candidate = input.slice(index - UNIQUE_CHAR_COUNT, index);
+    const uniqueChars = [...new Set(candidate)];
+    if (uniqueChars.length === UNIQUE_CHAR_COUNT) return [index, candidate.join('')];
   }
   throw new Error('Cant find marker in sequence');
 }
