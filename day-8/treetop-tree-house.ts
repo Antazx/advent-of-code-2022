dcffb9033dbecfb2da752b90d64273a4e558367c
Guillermo Anta <guillermo.anta@telecomboutique.com>
2022-12-15 14:33:56 +0100

    Day8.2 second star

--- day-8/treetop-tree-house.ts
+++ day-8/treetop-tree-house.ts
@@ -2,21 +2,20 @@ import readInput from '../readInput.ts';
 
 const input = await readInput('day-8/input.txt');
 const treeGrid = parseInput(input);
-console.log(`file: treetop-tree-house.ts:5 - treeGrid`, treeGrid);
 const visibleTrees = getVisibleTrees(treeGrid);
 console.log(`file: treetop-tree-house.ts:7 - visibleTrees`, visibleTrees);
 
 function getVisibleTrees(treeGrid: number[][]) {
-  let visibleTreeCount = 0;
+  let maxVisibility = 0;
+  const visibilities: number[][] = [];
   for (let rowIndex = 1; rowIndex < treeGrid.length - 1; rowIndex++) {
+    visibilities.push([]);
     for (let colIndex = 1; colIndex < treeGrid[rowIndex].length - 1; colIndex++) {
-      const visible = isTreeVisible(treeGrid, rowIndex, colIndex);
-      visibleTreeCount += visible;
+      const visibility = isTreeVisible(treeGrid, rowIndex, colIndex);
+      maxVisibility = Math.max(maxVisibility, visibility);
     }
   }
-  const edgeTreeCount = treeGrid.length * 2 + (treeGrid[0].length - 2) * 2;
-  console.log(`file: treetop-tree-house.ts:19 - edgeTreeCount`, edgeTreeCount);
-  return visibleTreeCount + edgeTreeCount;
+  return maxVisibility;
 }
 
 function isTreeVisible(treeGrid: number[][], row: number, col: number): number {
@@ -24,40 +23,46 @@ function isTreeVisible(treeGrid: number[][], row: number, col: number): number {
   const maxRowCount = treeGrid.length;
   const maxColCount = treeGrid[0].length;
 
-  let visibleFromAbove = true;
-  let visibleFromBelow = true;
-  let visibleFromRigth = true;
-  let visibleFromLeft = true;
+  let blocked = false;
+  let visibleCountFromBelow = 0;
+  let visibleCountFromAbove = 0;
+  let visibleCountFromRigth = 0;
+  let visibleCountFromLeft = 0;
 
   let tmpRow = row + 1;
-  while (tmpRow < maxRowCount) {
-    visibleFromBelow &&= currentTreeHeigth > treeGrid[tmpRow][col];
+  while (tmpRow < maxRowCount && !blocked) {
+    visibleCountFromBelow++;
+    blocked = currentTreeHeigth <= treeGrid[tmpRow][col];
     tmpRow++;
   }
-  if (visibleFromBelow) return 1;
 
+  blocked = false;
   tmpRow = row - 1;
-  while (tmpRow > -1) {
-    visibleFromAbove &&= currentTreeHeigth > treeGrid[tmpRow][col];
+  while (tmpRow > -1 && !blocked) {
+    visibleCountFromAbove++;
+    blocked = currentTreeHeigth <= treeGrid[tmpRow][col];
     tmpRow--;
   }
-  if (visibleFromAbove) return 1;
 
+  blocked = false;
   let tmpCol = col + 1;
-  while (tmpCol < maxColCount) {
-    visibleFromRigth &&= currentTreeHeigth > treeGrid[row][tmpCol];
+  while (tmpCol < maxColCount && !blocked) {
+    visibleCountFromRigth++;
+    blocked = currentTreeHeigth <= treeGrid[row][tmpCol];
     tmpCol++;
   }
-  if (visibleFromRigth) return 1;
 
+  blocked = false;
   tmpCol = col - 1;
-  while (tmpCol > -1) {
-    visibleFromLeft &&= currentTreeHeigth > treeGrid[row][tmpCol];
+  while (tmpCol > -1 && !blocked) {
+    visibleCountFromLeft++;
+    blocked = currentTreeHeigth <= treeGrid[row][tmpCol];
     tmpCol--;
   }
-  if (visibleFromLeft) return 1;
 
-  return 0;
+  const totalCount =
+    visibleCountFromAbove * visibleCountFromBelow * visibleCountFromLeft * visibleCountFromRigth;
+  return totalCount;
 }
 
 function parseInput(input: string): number[][] {
